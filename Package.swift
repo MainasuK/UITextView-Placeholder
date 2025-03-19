f90ce9a6698258852a463e74a4fb3c5895e1408b
CMK <cirno.mainasuk@gmail.com>
2025-03-19 15:11:12 +0800

    chore: update toolchain version to 5.7

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -15,6 +15,6 @@ let package = Package(
         .target(
             name: "UITextView+Placeholder",
             path: "Sources",
-            publicHeadersPath: ""),
+            publicHeadersPath: "include"),
     ]
 )
